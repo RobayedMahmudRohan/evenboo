aaf03548337af160da5c3501cea2d22fce7dd269
Saumik37 <saumik389@gmail.com>
2025-07-28 00:52:19 +0600

    Lab2

--- src/Organizer/org.service.ts
+++ src/Organizer/org.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { OrgData } from './org.dto';
 
 @Injectable()
 export class OrgService {
-    getOrg(): string {
-    return 'Hello Organizer!';
+    getAllOrg(): string {
+    return 'All Organizers!';
   }
 
-  getOrgID(id:number): string{
-    return 'aet Organizer id ' + id;
+  getOrgByID(id:number): string{
+    return 'get Organizer id ' + id;
+  }
+
+  createOrg(orgData: OrgData): string {
+    return 'Organizer created with OrgId: ' + orgData.orgId + ' and email: ' + orgData.email;
+  }
+
+  updateOrg(id: number, orgData: OrgData): string {
+    return 'Organizer updated with id: ' + id + ' and email: ' + orgData.email;
+  }
+
+  addOrg(orgData: OrgData, filename: string): string {
+    return 'Organizer added with OrgId: ' + orgData.orgId + ', name: ' + orgData.name + ', email: ' + orgData.email + ', file: ' + filename;
   }
 }
